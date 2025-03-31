800aa92400c80460656ac864fc7e5c7ed6c801ac
Valentyn <valentindikhtyarenko2004@gmail.com>
2025-03-31 19:28:41 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 500,
+    // slidesPerView: 2,
+    // centeredSlides: true,
+    // initialSlide: 1,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+   
+  });
